fd58f345bbb69291f714bb9b7772f67f575ca63c
khoabt94 <khoa.bui@buymed.com>
2024-09-12 09:31:41 +0700

    refactor: serialize interceptor into decorator

---
+++ server/src/decorators/serialize.decorator.ts
@@ -0,0 +1,7 @@
+import { UseInterceptors } from "@nestjs/common";
+import { ClassConstructor } from "class-transformer";
+import { SerializeInterceptor } from "src/interceptors/serialize.interceptor";
+
+export function Serialize(dto: ClassConstructor<any>) {
+  return UseInterceptors(new SerializeInterceptor(dto))
+}
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -3,12 +3,13 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 import { ResponseUserDto } from './dto/user-response.dto';
+import { Serialize } from 'src/decorators/serialize.decorator';
 
 @Controller('auth')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
-  @UseInterceptors(new SerializeInterceptor(ResponseUserDto))
+  @Serialize(ResponseUserDto)
   @Post('signup')
   signup(@Body() body: CreateUserDto) {
     return this.userService.create(body)
